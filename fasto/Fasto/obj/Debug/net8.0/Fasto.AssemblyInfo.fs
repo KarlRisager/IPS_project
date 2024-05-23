8e62c6702e6b152589cd59d041f78c06717a6768
Karl Skarsholm Risager <83304236+KarlRisager@users.noreply.github.com>
2024-05-23 17:21:39 +0200

    mul and div interpret

--- fasto/Fasto/obj/Debug/net8.0/Fasto.AssemblyInfo.fs
+++ fasto/Fasto/obj/Debug/net8.0/Fasto.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4de37c1e71eee212373068b59b4057445a0209")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe2287e6f99ef6dbd2a5c5fa0fecb9a5d8b1c2b")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Fasto")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
